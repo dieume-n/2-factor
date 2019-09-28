e4a3ba7454e9d01e47e72bbc6b60d0db35fd2eb5
= <=>
2019-09-28 12:42:34 +0300

    initial commit

---
+++ app/config/config.php
@@ -0,0 +1,21 @@
+<?php
+
+// DB Params
+define('DB_HOST', '127.0.0.1');
+define('DB_USER', 'root');
+define('DB_PASS', 'TKNp2hY2W2');
+define('DB_NAME', 'two_factor');
+
+define('AUTHY_API', 'KcCIZll3VsC63k1MLIuWIH3rEHvnCAlW');
+
+// App Root
+define('APPROOT', dirname(dirname(__FILE__)));
+
+// URL Root
+define('URLROOT', 'https://2-factor.local');
+
+// Site Name
+define('SITENAME', '2-Factor Authentication');
+
+// App Version
+define('APPVERSION', '1.0.0');
